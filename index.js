512b667721d5a82e27b53e51d537df206a5b299c
Duke Ovie <dukeovie09@gmail.com>
2019-09-04 23:43:27 +0200

    added the drum image backgrounds

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var numOfDrumButtons = document.querySelectorAll(".drum").length;
 
+
 for (var i = 0; i < numOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+
     });
 
 }
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
